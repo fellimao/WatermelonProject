2b37cf3d3b3f4d23c2f3726087dedc081a643bde
fellimao <fellimao.almeida@gmail.com>
2018-08-11 19:29:00 -0300

    Commit inicial - arquivos base com barra de navegaçcao

---
+++ Index.php
@@ -0,0 +1,33 @@
+<html lang="pt/br">
+    <head>
+        <title>Insira o titulo aqui</title>
+    	<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fontes -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+        <!--Ícone da página -->
+        <link rel="icon" href="img/Logo.png">
+        <!-- Conexão com o bootstrap/javascript/jquery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/form.js"></script>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+          <a class="navbar-brand" href="#">
+            <img src="img/logomarca.png" alt="Logo" style="width:40px;">
+          </a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </nav>
+
+    </body>
+</html>
